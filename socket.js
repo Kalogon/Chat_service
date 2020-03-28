6012a28a430f17236f50ce3f10175e24895b7547
Kalogon <ghlee5084@kaist.ac.kr>
2020-03-29 01:13:25 +0900

    add(username&details)

--- socket.js
+++ socket.js
@@ -1,13 +1,21 @@
+const username = ["Jack", "Lukas", "James", "Oliver", "Sophia", "Emma", "Aria", "Amelia"]
+
+
 module.exports = (io) => {
     io.on('connection', (socket) => {
         console.log('connected');
+        const user = username[Math.floor(Math.random()*username.length)]
         
+        io.to(socket.id).emit('name', user)
+        socket.broadcast.emit('enter', user)
+
         socket.on('disconnect', function(){
             console.log('disconnected')
         })
+
         socket.on('chat message', function(message){
             console.log("send")
-            socket.broadcast.emit('chat message', message)
+            socket.broadcast.emit('chat message', user, message)
         })
     });
   };
