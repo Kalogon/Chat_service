a14d2947034ea5f533fecb788b1855490384b7d8
Kalogon <ghlee5084@kaist.ac.kr>
2020-03-28 22:18:38 +0900

    start-project

---
+++ socket.js
@@ -0,0 +1,13 @@
+module.exports = (io) => {
+    console.log("일단")
+    io.on('connection', (socket) => { // 웹소켓 연결 시
+        console.log('Socket initiated!');
+        socket.on('newScoreToServer', (data) => { // 클라이언트에서 newScoreToServer 이벤트 요청 시
+            console.log('Socket: newScore');
+            io.emit('newScoreToClient', data);
+        });
+        socket.on('disconnect', function(){
+            console.log('user disconnected')
+        })
+    });
+  };
